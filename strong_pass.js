100def34610c09764cc1db22b3e9a450eb496e2b
kenpeter <kenpeter@kenpeter>
2018-08-12 03:00:50 +0000

    update

--- strong_pass.js
+++ strong_pass.js
@@ -23,7 +23,7 @@ function hasUp(str) {
 }
 
 function hasSpe(str) {
-	if(/[!@#$%^&*()-+]/.test(str))
+	if(/[!@#$%^&*()\-+]/.test(str))
 		return 1;
 	else
 		return 0;
@@ -31,20 +31,22 @@ function hasSpe(str) {
 
 function mymin(p) {
 	var curr_len = p.length;
-	
-	var type_len = hasNum(p) + hasLo(p) + hasUp(p) + hasSpe(p);
-	var type_miss_len = 4 - type_len;
-	var fill_len;
-	var min_p_len = 6;
-	var need_len = 0;
-	if(curr_len < min_p_len) {
-		fill_len = min_p_len - (curr_len + type_miss_len);
-		need_len = fill_len + type_miss_len;	
-	} else {
-		need_len = type_miss_len;
-	}			
 
-	return need_len;
+	var all_type = 4;
+	var min_p_len = 6;	
+	
+	var type_exist_len = hasNum(p) + hasLo(p) + hasUp(p) + hasSpe(p);
+	var type_miss_len = all_type - type_exist_len;  	
+	var after_add_miss_type_len = curr_len + type_miss_len;
+
+	// e.g. abc + 1A$
+	if(after_add_miss_type_len >= min_p_len) {
+		return type_miss_len;
+	} else {
+		// e.g. a + 1A$, so (fill) + miss_type 
+		return (min_p_len - after_add_miss_type_len) + type_miss_len;
+	}
+
 }
 
 function minimumNumber(n, password) {
@@ -52,6 +54,6 @@ function minimumNumber(n, password) {
 }
 
 var n = null;
-var password = 'Ab1'; 
+var password = 'AUzs-nV'; 
 var out = minimumNumber(n, password);
 console.log(out);
